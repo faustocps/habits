379f95765d10be2d035886eb73d04261d495f8a3
Fausto Candido <fausto.cps@hotmail.com>
2023-01-21 17:41:09 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { appRoutes } from "./routes";
+
+const app = Fastify();
+
+app.register(cors);
+app.register(appRoutes);
+
+/**
+ * Method HTTP: Get, Post, Put, Patch, Delete
+ */
+
+app
+  .listen({
+    host: "0.0.0.0",
+    port: 3333,
+  })
+  .then(() => console.log("HTTP Server running!"));
---
+++ web/src/App.tsx
@@ -0,0 +1,23 @@
+import { Header } from './compenents/Header';
+import { SummaryTable } from './compenents/SummaryTable';
+import './styles/global.css';
+import './lib/dayjs'
+// import { Habit } from "./compenents/Habit"
+
+
+export function App() {
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+          
+         <Header/>
+
+         <SummaryTable/>
+
+      </div>
+      
+    </div>
+  )
+}
+
